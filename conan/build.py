e12c97e59a3b3f8af34dd5becb2df8120243f4a7
Uilian Ries <uilianries@gmail.com>
2019-03-06 22:45:13 -0300

    #109 Add Conan package tools
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ conan/build.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+from cpt.packager import ConanMultiPackager
+from cpt.ci_manager import CIManager
+from cpt.printer import Printer
+from cpt import tools
+
+
+class BuilderSettings(object):
+
+    def __init__(self):
+        self._ci_manager = CIManager(Printer(None))
+
+    @property
+    def branch(self):
+        """ Get branch name
+        """
+        return self._ci_manager.get_branch()
+
+    @property
+    def is_tag(self):
+        """ Is the current branch a tag
+        """
+        return self._ci_manager.is_tag()
+
+    @property
+    def commit_id(self):
+        """ Get current commit hash
+        """
+        return self._ci_manager.get_commit_id()[:7]
+
+    @property
+    def channel(self):
+        """ Get channel name
+        """
+        return "stable" if self.is_tag else "testing"
+
+    @property
+    def username(self):
+        """ Set simdjson as package's owner
+        """
+        return os.getenv("CONAN_USERNAME", "lemire")
+
+    @property
+    def upload(self):
+        """ Set simdjson repository to be used on upload.
+            The upload server address could be customized by env var
+            CONAN_UPLOAD. If not defined, the method will check if the
+            current branch is a tag.
+        """
+        if os.getenv("CONAN_UPLOAD", None) is not None:
+            return os.getenv("CONAN_UPLOAD")
+
+        if self.is_tag:
+            return "https://api.bintray.com/conan/lemire/conan"
+
+        return None
+
+    @property
+    def upload_only_when_stable(self):
+        """ Force to upload when match stable pattern branch
+        """
+        return os.getenv("CONAN_UPLOAD_ONLY_WHEN_STABLE", "true").lower() in ["true", "1", "y", "yes"]
+
+    @property
+    def version(self):
+        """ Return the package version based on branch name or
+            the commit id if it is not a tag.
+        """
+        if self.is_tag:
+            return re.match(r"v(\d+\.\d+)", self.branch).group(1)
+        return self.commit_id
+
+    @property
+    def reference(self):
+        """ Read project version from branch name to create Conan referece
+        """
+        return os.getenv("CONAN_REFERENCE", "simdjson/{}".format(self.version))
+
+if __name__ == "__main__":
+    settings = BuilderSettings()
+    builder = ConanMultiPackager(
+        reference=settings.reference,
+        username=settings.username,
+        upload=settings.upload,
+        upload_only_when_stable=settings.upload_only_when_stable,
+        channel=settings.channel,
+        test_folder=os.path.join("conan", "test_package"))
+    builder.add_common_builds(pure_c=False)
+    if os.getenv("CONAN_CLANG_VERSIONS", False) == "7.0":
+        builder.add({}, {"simdjson:header_only" : True}, {}, {})
+    builder.run()
